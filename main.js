b75902b0b589bbe831aa8669f3de6a94af459cfe
walisson150 <130377955+walisson150@users.noreply.github.com>
2024-08-27 10:58:43 -0300

    salvando

--- main.js
+++ main.js
@@ -3,3 +3,27 @@ const caixaPergunta = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+
+
+
+
+];
