05836d7c074f745dd625b958e21ca3b90b073006
walisson150 <130377955+walisson150@users.noreply.github.com>
2024-08-27 07:47:07 -0300

    caixas

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPrincipal = document.querySelector(".caixa-pergunta");
+const caixaPrincipal = document.querySelector(".caixa-perguntas");
 const caixaPrincipal = document.querySelector(".caixa-alternativas");
 const caixaPrincipal = document.querySelector(".caixa-resultado");
 const caixaPrincipal = document.querySelector(".texto-resultado");
